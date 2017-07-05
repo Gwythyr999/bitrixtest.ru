950ef209e147a73d98d5215a089175a97fc027c3
Gwythyr999 <filipenko@ctweb.ru>
2017-07-05 16:25:39 +0700

    some new

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001499245157';
-$dateexpire = '001499248817';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1499245198;}';
-return true;
-?>
